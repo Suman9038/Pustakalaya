4a3f9dea32ff8feb2dcaaa244148a73fc8899be1
Suman9038 <sumanjhad903878@gmail.com>
2026-08-18 17:09:09 +0530

    Fixed RAM OOM issue with lazy imports

--- app/rag/embeddings.py
+++ app/rag/embeddings.py
@@ -1,5 +1,3 @@
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from langchain_huggingface import HuggingFaceEmbeddings,HuggingFacePipeline
 from dotenv import load_dotenv
 load_dotenv()
 from app.config import settings
@@ -11,6 +9,7 @@ class GetEmbeddingModels:
 
     @classmethod
     def get_google_embedding_models(cls):
+        from langchain_google_genai import GoogleGenerativeAIEmbeddings
         return GoogleGenerativeAIEmbeddings(
             model="gemini-embedding-001",
         )
@@ -18,6 +17,7 @@ class GetEmbeddingModels:
     @classmethod
     def get_hf_embedding_models(cls):
         if cls._hf_model is None:
+            from langchain_huggingface import HuggingFaceEmbeddings
             cls._hf_model = HuggingFaceEmbeddings(
                model_name="ibm-granite/granite-embedding-97m-multilingual-r2",
                cache_folder=".embeddings/.cache"
